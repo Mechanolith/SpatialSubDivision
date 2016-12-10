c76ba7b5440403c272dbd1efdc18fe767cde5bf9
Mechanolith <j.kuskoff@hotmail.com>
2016-12-10 19:16:36 +1000

    Basic Quadrant Optimisation
    Rendering is now optimised using quadrants.
    Rendering is now ~4x faster.
    
    Code is messy and inefficient.
    SIGNIFICANT CODE CLEAN-UP REQUIRED.

--- spatial/main.cpp
+++ spatial/main.cpp
@@ -18,7 +18,10 @@ public:
 	float m_radius;
 };
 
-std::vector<Circle> g_circles;
+std::vector<Circle> g_circles0;	//Top Left
+std::vector<Circle> g_circles1;	//Top Right
+std::vector<Circle> g_circles2;	//Bottom Left
+std::vector<Circle> g_circles3;	//Bottom Right
 
 
 int main(int argc, char **argv)
@@ -36,27 +39,120 @@ int main(int argc, char **argv)
 		c.m_pos.x = rand() % g_windowWidth;
 		c.m_pos.y = rand() % g_windowHeight;
 		c.m_radius = rand() % 50;
-		g_circles.push_back(c);
+
+		float hwidth = g_windowWidth / 2;
+		float hheight = g_windowHeight / 2;
+
+		//Check if it's in the top left quadrant
+		if(c.m_pos.x < hwidth && c.m_pos.y < hheight)
+		{
+			g_circles0.push_back(c);
+
+			//Calculate overlaps.
+			if (c.m_pos.x + c.m_radius > hwidth)
+			{
+				g_circles1.push_back(c);
+			}
+
+			if(c.m_pos.y + c.m_radius > hheight)
+			{
+				g_circles2.push_back(c);
+			}
+
+			if ((c.m_pos.x + c.m_radius > hwidth) && (c.m_pos.y + c.m_radius > hheight))
+			{
+				g_circles3.push_back(c);
+			}
+		}	
+		//Top Right Quad
+		else if (c.m_pos.x >= hwidth && c.m_pos.y < hheight)
+		{
+			g_circles1.push_back(c);
+
+			//Calculate overlaps.
+			if (c.m_pos.x - c.m_radius < hwidth)
+			{
+				g_circles0.push_back(c);
+			}
+
+			if (c.m_pos.y + c.m_radius > hheight)
+			{
+				g_circles3.push_back(c);
+			}
+
+			if ((c.m_pos.x - c.m_radius < hwidth) && (c.m_pos.y + c.m_radius > hheight))
+			{
+				g_circles2.push_back(c);
+			}
+		}
+		//Bottom Left Quad
+		else if (c.m_pos.x < hwidth && c.m_pos.y >= hheight)
+		{
+			g_circles2.push_back(c);
+
+			//Calculate overlaps.
+			if (c.m_pos.x + c.m_radius > hwidth)
+			{
+				g_circles3.push_back(c);
+			}
+
+			if (c.m_pos.y - c.m_radius < hheight)
+			{
+				g_circles0.push_back(c);
+			}
+
+			if ((c.m_pos.x + c.m_radius > hwidth) && (c.m_pos.y - c.m_radius < hheight))
+			{
+				g_circles1.push_back(c);
+			}
+		}
+		//Bottom Right Quad
+		else if (c.m_pos.x >= hwidth && c.m_pos.y >= hheight)
+		{
+			g_circles3.push_back(c);
+
+			//Calculate overlaps.
+			if (c.m_pos.x - c.m_radius < hwidth)
+			{
+				g_circles2.push_back(c);
+			}
+
+			if (c.m_pos.y - c.m_radius < hheight)
+			{
+				g_circles1.push_back(c);
+			}
+
+			if ((c.m_pos.x - c.m_radius < hwidth) && (c.m_pos.y - c.m_radius < hheight))
+			{
+				g_circles0.push_back(c);
+			}
+		}
 	}
 
 
 	// Record the starting time.
 	DWORD startTime = timeGetTime();
 
-	// Primary loop through all screen pixels.
-	for (int y = 0; y < g_windowHeight; ++y)
+	//Top Left Loop
+	for (int y = 0; y < g_windowHeight / 2; ++y) 
 	{
-		for (int x = 0; x < g_windowWidth; ++x)
+		for (int x = 0; x < g_windowWidth / 2; ++x) 
 		{
-			kf::Colour output(0,0,0,0);
+			kf::Colour output(0, 0, 0, 0);
 			float e = 0;
-			for (int i = 0; i < g_circles.size(); ++i)
+			for (int i = 0; i < g_circles0.size(); ++i)
 			{
-				float d = (kf::Vector2(x, y) - g_circles[i].m_pos).length();
-				if (d < g_circles[i].m_radius)
-				{
-					e = e + (1.0-d / g_circles[i].m_radius);
-				}
+				//float dx = abs(g_circles[i].m_pos.x - x);
+				//float dy = abs(g_circles[i].m_pos.y - y);
+
+				//if ((dx *dx) + (dy*dy) <= g_circles[i].m_radius * g_circles[i].m_radius)
+				//{
+					float d = (kf::Vector2(x, y) - g_circles0[i].m_pos).length();
+					if (d < g_circles0[i].m_radius)
+					{
+						e = e + (1.0 - d / g_circles0[i].m_radius);
+					}
+				//}
 			}
 			output.set(e, e, e, 1);
 
@@ -65,9 +161,123 @@ int main(int argc, char **argv)
 			// Convert from linear space to sRGB.
 			output.toSRGB();
 			// Write the colour to the image (scaling up by 255).
-			*image.data(x, y, 0, 0) = output.r*255;
-			*image.data(x, y, 0, 1) = output.g*255;
-			*image.data(x, y, 0, 2) = output.b*255;
+			*image.data(x, y, 0, 0) = output.r * 255;
+			*image.data(x, y, 0, 1) = output.g * 255;
+			*image.data(x, y, 0, 2) = output.b * 255;
+		}
+		std::cout << y << std::endl;
+		// Check for Escape key.
+		if (main_disp.is_keyESC())
+			return 0;
+	}
+
+	//Top Right Loop
+	for (int y = 0; y < g_windowHeight / 2; ++y)
+	{
+		for (int x = g_windowWidth / 2; x < g_windowWidth; ++x)
+		{
+			kf::Colour output(0, 0, 0, 0);
+			float e = 0;
+			for (int i = 0; i < g_circles1.size(); ++i)
+			{
+				//float dx = abs(g_circles[i].m_pos.x - x);
+				//float dy = abs(g_circles[i].m_pos.y - y);
+
+				//if ((dx *dx) + (dy*dy) <= g_circles[i].m_radius * g_circles[i].m_radius)
+				//{
+				float d = (kf::Vector2(x, y) - g_circles1[i].m_pos).length();
+				if (d < g_circles1[i].m_radius)
+				{
+					e = e + (1.0 - d / g_circles1[i].m_radius);
+				}
+				//}
+			}
+			output.set(e, e, e, 1);
+
+			// Clamp the output colour to 0-1 range before conversion.
+			output.saturate();
+			// Convert from linear space to sRGB.
+			output.toSRGB();
+			// Write the colour to the image (scaling up by 255).
+			*image.data(x, y, 0, 0) = output.r * 255;
+			*image.data(x, y, 0, 1) = output.g * 255;
+			*image.data(x, y, 0, 2) = output.b * 255;
+		}
+		std::cout << y << std::endl;
+		// Check for Escape key.
+		if (main_disp.is_keyESC())
+			return 0;
+	}
+
+	//Bottom Left Loop
+	for (int y = g_windowHeight / 2; y < g_windowHeight; ++y)
+	{
+		for (int x = 0; x < g_windowWidth / 2; ++x)
+		{
+			kf::Colour output(0, 0, 0, 0);
+			float e = 0;
+			for (int i = 0; i < g_circles2.size(); ++i)
+			{
+				//float dx = abs(g_circles[i].m_pos.x - x);
+				//float dy = abs(g_circles[i].m_pos.y - y);
+
+				//if ((dx *dx) + (dy*dy) <= g_circles[i].m_radius * g_circles[i].m_radius)
+				//{
+				float d = (kf::Vector2(x, y) - g_circles2[i].m_pos).length();
+				if (d < g_circles2[i].m_radius)
+				{
+					e = e + (1.0 - d / g_circles2[i].m_radius);
+				}
+				//}
+			}
+			output.set(e, e, e, 1);
+
+			// Clamp the output colour to 0-1 range before conversion.
+			output.saturate();
+			// Convert from linear space to sRGB.
+			output.toSRGB();
+			// Write the colour to the image (scaling up by 255).
+			*image.data(x, y, 0, 0) = output.r * 255;
+			*image.data(x, y, 0, 1) = output.g * 255;
+			*image.data(x, y, 0, 2) = output.b * 255;
+		}
+		std::cout << y << std::endl;
+		// Check for Escape key.
+		if (main_disp.is_keyESC())
+			return 0;
+	}
+
+	//Bottom Right Loop
+	for (int y = g_windowHeight / 2; y < g_windowHeight; ++y)
+	{
+		for (int x = g_windowHeight / 2; x < g_windowWidth; ++x)
+		{
+			kf::Colour output(0, 0, 0, 0);
+			float e = 0;
+			for (int i = 0; i < g_circles3.size(); ++i)
+			{
+				//float dx = abs(g_circles[i].m_pos.x - x);
+				//float dy = abs(g_circles[i].m_pos.y - y);
+
+				//if ((dx *dx) + (dy*dy) <= g_circles[i].m_radius * g_circles[i].m_radius)
+				//{
+				float d = (kf::Vector2(x, y) - g_circles3[i].m_pos).length();
+				if (d < g_circles3[i].m_radius)
+				{
+					e = e + (1.0 - d / g_circles3[i].m_radius);
+				}
+				//}
+			}
+			output.set(e, e, e, 1);
+
+			// Clamp the output colour to 0-1 range before conversion.
+			output.saturate();
+			// Convert from linear space to sRGB.
+			output.toSRGB();
+			// Write the colour to the image (scaling up by 255).
+			*image.data(x, y, 0, 0) = output.r * 255;
+			*image.data(x, y, 0, 1) = output.g * 255;
+			*image.data(x, y, 0, 2) = output.b * 255;
 		}
 		std::cout << y << std::endl;
 		// Check for Escape key.
